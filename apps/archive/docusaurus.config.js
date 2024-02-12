dbbe37ac7bb05e827a624eccd9bb690367b00666
BorysShulyak <borysshulyak@gmail.com>
2024-02-12 14:47:29 +0200

    ci(ci-mr): share build artifacts

--- apps/archive/docusaurus.config.js
+++ apps/archive/docusaurus.config.js
@@ -18,11 +18,12 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'BorysShulyak', // Usually your GitHub org/user name.
-  projectName: '@runespoorstack/archive', // Usually your repo name.
+  organizationName: 'runespoor-engineering', // Usually your GitHub org/user name.
+  projectName: 'runespoorstack', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
