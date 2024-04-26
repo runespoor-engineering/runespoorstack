1b155e2357cdf8cf95a75f9aebaec6264a964b90
BorysShulyak <borysshulyak@gmail.com>
2024-04-26 14:22:40 +0300

    docs(archive): add `Issues To Keep Track On` page

--- apps/archive/sidebars.js
+++ apps/archive/sidebars.js
@@ -28,6 +28,7 @@ const sidebars = {
           label: 'Source Control',
           items: [
             'engineering-playbook/source-control/tech-stack',
+            'engineering-playbook/source-control/issues-to-keep-track-on',
             'engineering-playbook/source-control/git-control',
             'engineering-playbook/source-control/labels',
             'engineering-playbook/source-control/changelog',
