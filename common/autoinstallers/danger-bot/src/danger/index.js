40977a2722e757567312af09100315a13ed8ec0b
BorysShulyak <borysshulyak@gmail.com>
2024-05-09 19:22:42 +0300

    chore(autoinstallers/danger-bot): Refactor danger-bot workflow to use index.js module

--- common/autoinstallers/danger-bot/src/danger/index.js
+++ common/autoinstallers/danger-bot/src/danger/index.js
@@ -1,10 +1,10 @@
-const dangerAssignee = require('./plugins/dangerAssignee.js');
-const dangerChangelog = require('./plugins/dangerChangelog.js');
-const dangerLockfileUpdate = require('./plugins/dangerLockfileUpdate.js');
-const dangerPrMergingTime = require('./plugins/dangerPrMergingTime.js');
-const dangerPrSize = require('./plugins/dangerPrSize.js');
-const dangerPrTitle = require('./plugins/dangerPrTitle.js');
-const dangerReviewers = require('./plugins/dangerReviewers.js');
+const { dangerAssignee } = require('./plugins/dangerAssignee.js');
+const { dangerChangelog } = require('./plugins/dangerChangelog.js');
+const { dangerLockfileUpdate } = require('./plugins/dangerLockfileUpdate.js');
+const { dangerPrMergingTime } = require('./plugins/dangerPrMergingTime.js');
+const { dangerPrSize } = require('./plugins/dangerPrSize.js');
+const { dangerPrTitle } = require('./plugins/dangerPrTitle.js');
+const { dangerReviewers } = require('./plugins/dangerReviewers.js');
 
 module.exports = {
   dangerAssignee,
