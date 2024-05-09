ce5a666ad71d3b31ff9d8897815673da89a9c0e8
BorysShulyak <borysshulyak@gmail.com>
2024-05-09 19:35:10 +0300

    chore: Update danger-bot plugins to use warn instead of fail

--- common/autoinstallers/danger-bot/src/danger/plugins/dangerLockfileUpdate.js
+++ common/autoinstallers/danger-bot/src/danger/plugins/dangerLockfileUpdate.js
@@ -1,3 +1,5 @@
+const { warn } = require('danger');
+
 const TYPE_TO_LOCKFILE_NAME = {
   npm: 'package-lock.json',
   yarn: 'yarn.lock',
@@ -10,7 +12,7 @@ const TYPE_TO_LOCKFILE_NAME = {
  *
  * @param {string} type - The type of package manager used (e.g., 'npm', 'yarn', 'pnpm', 'rush').
  *
- * @returns {void} - No return value. If the conditions are met, it will fail the build
+ * @returns {void} - No return value. If the conditions are met, it will through a warning
  * with a message.
  */
 const dangerLockfileUpdate = ({ type }) => {
@@ -18,7 +20,7 @@ const dangerLockfileUpdate = ({ type }) => {
   const isPackageChanged = danger.git.modified_files.includes('package.json');
   const isLockfileChanged = danger.git.modified_files.includes(packageLockFileName);
   if (isPackageChanged && !isLockfileChanged) {
-    fail(
+    warn(
       `Changes were made to package.json, but not to ${packageLockFileName}. - <i>Perhaps you need to install/update dependencies?</i>`
     );
   }
