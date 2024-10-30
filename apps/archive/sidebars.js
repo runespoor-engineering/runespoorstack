ab7f069ba95045d0a95dc7b910ab79591af608e6
BorysShulyak <borysshulyak@gmail.com>
2024-10-30 16:52:41 +0200

    docs(ci/cd): add `production-hot-fixes` doc

--- apps/archive/sidebars.js
+++ apps/archive/sidebars.js
@@ -127,6 +127,7 @@ const sidebars = {
             'engineering-playbook/CI-CD/continuous-integration',
             'engineering-playbook/CI-CD/ci-cd-pipelines',
             'engineering-playbook/CI-CD/feature-flags',
+            'engineering-playbook/CI-CD/production-hot-fixes',
             'engineering-playbook/CI-CD/copy-paste-analyzing'
           ]
         },
