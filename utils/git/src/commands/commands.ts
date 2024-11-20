d314fd2e96030dd2cbf9ae29adf61014f6f0b258
BorysShulyak <borysshulyak@gmail.com>
2024-11-20 15:29:10 -0700

    feat(GIT_COMMANDS): add `add`, `commit`, `push` commands

--- utils/git/src/commands/commands.ts
+++ utils/git/src/commands/commands.ts
@@ -2,5 +2,8 @@ export const GIT_COMMANDS = {
   fetchOrigin: () => 'git fetch origin',
   commitsCountBetweenBranches: (from: string, to: string) => `git rev-list --count ${from}..${to}`,
   currentBranchName: () => 'git rev-parse --abbrev-ref HEAD',
-  defaultBranchName: () => 'git remote show origin | grep "HEAD branch" | cut -d" " -f5'
+  defaultBranchName: () => 'git remote show origin | grep "HEAD branch" | cut -d" " -f5',
+  add: (path: string = '.') => `git add ${path}`,
+  commit: (message: string) => `git commit -m "${message}"`,
+  push: () => 'git push'
 };
