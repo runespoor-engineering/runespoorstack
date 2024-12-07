674d54b29a4383ff924a5a9e1b8aeda3079f3797
BorysShulyak <borysshulyak@gmail.com>
2024-12-07 14:57:43 +0200

    fix

--- utils/git/src/getModifiedFiles/__tests__/getModifiedFiles.test.ts
+++ utils/git/src/getModifiedFiles/__tests__/getModifiedFiles.test.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
+import { Mock } from 'vitest';
 
 import { getModifiedFiles } from '../getModifiedFiles';
-import { Mock } from 'vitest';
 
 vi.mock('child_process', () => ({
   execSync: vi.fn()
@@ -9,26 +9,29 @@ vi.mock('child_process', () => ({
 
 describe('getModifiedFiles', () => {
   it('should return an array of modified files', () => {
+    expect.hasAssertions();
     const mockResult = 'file1.ts\nfile2.ts\nfile3.ts\n';
     (execSync as Mock).mockReturnValueOnce(mockResult);
 
     const result = getModifiedFiles();
 
     expect(execSync).toHaveBeenCalledWith('git diff --name-only');
-    expect(result).toEqual(['file1.ts', 'file2.ts', 'file3.ts']);
+    expect(result).toStrictEqual(['file1.ts', 'file2.ts', 'file3.ts']);
   });
 
   it('should return an empty array if no files are modified', () => {
+    expect.hasAssertions();
     const mockResult = '';
     (execSync as Mock).mockReturnValueOnce(mockResult);
 
     const result = getModifiedFiles();
 
     expect(execSync).toHaveBeenCalledWith('git diff --name-only');
-    expect(result).toEqual([]);
+    expect(result).toStrictEqual([]);
   });
 
   it('should handle errors and return an empty array', () => {
+    expect.hasAssertions();
     const mockError = new Error('Command failed');
     (execSync as Mock).mockImplementationOnce(() => {
       throw mockError;
@@ -41,6 +44,6 @@ describe('getModifiedFiles', () => {
 
     expect(execSync).toHaveBeenCalledWith('git diff --name-only');
     expect(consoleErrorMock).toHaveBeenCalledWith('Error getting modified files:', mockError);
-    expect(result).toEqual([]);
+    expect(result).toStrictEqual([]);
   });
 });
