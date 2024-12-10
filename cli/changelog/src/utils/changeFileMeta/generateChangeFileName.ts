bc7fa8d037f522caa870327c32caa01c779e7a1a
Boris Shulyak <55171497+BorysShulyak@users.noreply.github.com>
2024-12-10 02:15:51 +0200

    fix(generateChangeFileName): replace `/` with `-` symbol in branch name (#202)

--- cli/changelog/src/utils/changeFileMeta/generateChangeFileName.ts
+++ cli/changelog/src/utils/changeFileMeta/generateChangeFileName.ts
@@ -2,9 +2,12 @@ import { execSync } from 'node:child_process';
 
 import { GIT_COMMANDS } from '../git/command';
 
+const NOT_ALLOWED_FILE_SYMBOLS_REGEX = /\//g;
+
 export const generateChangeFileName = () => {
   const branchName = execSync(GIT_COMMANDS.currentBranchName()).toString().trim();
   const timestamp = new Date().toISOString().replace(/[:.T]/g, '-').slice(0, -2);
-  const changeFileName = `${branchName}_${timestamp}.json`;
+  const formattedBranchName = branchName.replaceAll(NOT_ALLOWED_FILE_SYMBOLS_REGEX, '-');
+  const changeFileName = `${formattedBranchName}_${timestamp}.json`;
   return changeFileName;
 };
