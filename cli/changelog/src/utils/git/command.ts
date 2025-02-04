8c0284c6fc43495a76daef4230a9937a8684f98b
BorysShulyak <borysshulyak@gmail.com>
2025-02-04 10:29:57 +0200

    fix

--- cli/changelog/src/utils/git/command.ts
+++ cli/changelog/src/utils/git/command.ts
@@ -7,6 +7,7 @@ export const GIT_COMMANDS = {
     `git remote show ${remote} | grep "HEAD branch" | cut -d" " -f5`,
   add: (path: string = '.') => `git add ${path}`,
   commit: (message: string) => `git commit -m "${message}"`,
-  push: (origin: string, branch: string) => `git push --set-upstream ${origin} ${branch} --no-verify`,
+  push: (origin: string, branch: string) =>
+    `git push --set-upstream ${origin} ${branch} --no-verify`,
   configUserName: () => 'git config user.name'
 };
