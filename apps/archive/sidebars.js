0d32ffe15e4dda8bd8a3df4ee058c77eaea283bf
BorysShulyak <borysshulyak@gmail.com>
2025-03-26 09:41:04 +0100

    docs: enhance productivity destruction meetings documentation with detailed insights and visuals

--- apps/archive/sidebars.js
+++ apps/archive/sidebars.js
@@ -78,7 +78,8 @@ const sidebars = {
             'engineering-playbook/processes/critical-chain',
             'engineering-playbook/processes/delayIn-managerial-decision-making',
             'engineering-playbook/processes/tech-interview',
-            'engineering-playbook/processes/onboarding'
+            'engineering-playbook/processes/onboarding',
+            'engineering-playbook/processes/productivity-desctruction-meetings'
           ]
         },
         {
