8a292dd83496f4a6be718b3d3d2af25919422f05
BorysShulyak <borysshulyak@gmail.com>
2025-04-20 09:11:03 +0200

    feat: export additional RGBA utility functions for extraction and validation from color module

--- utils/color/src/index.ts
+++ utils/color/src/index.ts
@@ -1,3 +1,5 @@
 export * from './constants/regexp';
 export * from './types';
+export { extractRgbaObjectFromString } from './utils/extractRgbaObjectFromString/extractRgbaObjectFromString';
 export { rgbaStringToObject } from './utils/rgbaStringToObject/rgbaStringToObject';
+export { validateRgbaString } from './validation/validateRgbaString/validateRgbaString';
