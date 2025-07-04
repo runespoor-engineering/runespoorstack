ffbbc2a8af0ddfee1566d48850d63b0cf29e7a61
BorysShulyak <borysshulyak@gmail.com>
2025-07-04 15:21:39 +0200

    fix(checkStaged): add missing newline at end of file

--- cli/eslint/src/commands/checkStaged.ts
+++ cli/eslint/src/commands/checkStaged.ts
@@ -24,4 +24,4 @@ export const checkStaged = async (options: { rootDir?: string; pattern?: string[
     console.error(error instanceof Error ? error.message : String(error));
     process.exit(1);
   }
-}; 
+};
